e02d58af3d7698fff08966db7d437c8c20bbbb0c
Lwin Phyo <lwinphyo271395@gmail.com>
2025-01-08 12:52:52 +0700

    practice1.2 dataProps & binding done

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <h1 ref="title">
+    helloAll
+  </h1>
+
+  <input type="text" ref="focus">
+  <button @click="clickHandler" ref="name">click</button>
+
+  <AppModal 
+  :header="header"
+  :content="content"
+  :theme="theme" ></AppModal>
+
+</template>
+
+
+
+<script>
+import AppModal from './components/AppModal.vue';
+
+export default {
+
+  data(){
+    return {
+      header: "Login Success",
+      content: "Welocome all users",
+      theme: "success"
+      // theme: "fail"
+    }
+  },
+
+  components: { AppModal },
+ 
+  methods:{
+    clickHandler(){
+      // console.log(this.$refs.name)
+      this.$refs.name.textContent = "clicked",
+      this.$refs.title.textContent = "Hi there, changed it",
+      this.$refs.focus.focus()
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppModal.vue
@@ -0,0 +1,61 @@
+<template>
+
+    <div class="modal" 
+    :class="{success: theme ==='success' , fail: theme === 'fail' }">
+        <h2>{{ header }}</h2>
+        <p>{{ content }}</p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+   props: ['header', 'content', 'theme']
+
+}
+</script>
+
+<style scoped>
+
+    .modal {
+        background-color: #f4ae16;
+        padding: 20px;
+        border-radius: 12px;
+        text-align: center;
+        box-shadow: 0 6px 12px rgba(75, 76, 76, 0.632);
+        transition: all 0.3s ease;
+        max-width: 500px;
+        margin: auto;
+        width: 60%;
+    }
+
+    /* Add a title animation for the modal appearance */
+    .modal {
+        animation: fadeIn 0.5s ease-out;
+    }
+
+    /* Modal fade-in effect */
+    @keyframes fadeIn {
+        from {
+            opacity: 0;
+            transform: translateY(-10px);
+        }
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+    .success {
+        background-color: #017364;
+        color: #fff;
+
+
+    }
+
+    .fail {
+        background-color: #e91e63;
+        color: #fff;
+    }
+
+</style>
